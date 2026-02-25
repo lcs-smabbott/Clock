fd3ffe445aab0fcf2876bfe0a65aef3b3e91743d
Sadie Mabbott <smabbott@lcs.on.ca>
2026-02-25 10:56:17 -0500

    Added padding and second title to alarms view

--- Clock/Views/AlarmsView.swift
+++ Clock/Views/AlarmsView.swift
@@ -9,10 +9,60 @@ import SwiftUI
 
 struct AlarmsView: View {
     var body: some View {
-        Text("Alarms")
+        NavigationStack {
+            VStack (alignment: .leading) {
+                Text("Sleep | Wake Up")
+                    .padding(.horizontal)
+                ExtractedView(time: "7:00", amOrPm: "AM")
+                ExtractedView(time: "8:15", amOrPm: "AM")
+                ExtractedView(time: "9:00", amOrPm: "AM")
+                ExtractedView(time: "9:15", amOrPm: "AM")
+            }
+            .navigationTitle("Alarms")
+            .toolbar {
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    
+                    Button("Edit") {
+                        // Does nothing right now
+                    }
+                    
+                }
+                
+                ToolbarItem(placement: .primaryAction) {
+                    Button {
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    AlarmsView()
+    LandingView()
+}
+
+struct ExtractedView: View {
+    
+    let time: String
+    let amOrPm: String
+    
+    var body: some View {
+        HStack {
+            //left side
+            Text(time)
+                .font(.system(size: 64.0, weight: .thin, design: .default))
+            Text(amOrPm)
+                .font(.system(.largeTitle, design: .default, weight: .thin))
+            
+            Spacer()
+            
+            //Right side
+            Toggle("", isOn: Binding.constant(true))
+        }
+        .padding(.horizontal)
+        .tint(.green)
+    }
 }
--- Clock/Views/WorldClockView.swift
+++ Clock/Views/WorldClockView.swift
@@ -14,35 +14,36 @@ struct WorldClockView: View {
                 // Ottawa
                 CityView(timeZoneOffset: "+0", city: "Ottawa", time: "6:35", amOrPm: "AM")
                 //Vancouver
-                    CityView(timeZoneOffset: "-3", city: "Vancouver", time: "3:35", amOrPm: "AM")
-                    //Hamburg
-                    CityView(timeZoneOffset: "+6", city: "Hamburg", time: "12:35", amOrPm: "PM")
+                CityView(timeZoneOffset: "-3", city: "Vancouver", time: "3:35", amOrPm: "AM")
+                //Hamburg
+                CityView(timeZoneOffset: "+6", city: "Hamburg", time: "12:35", amOrPm: "PM")
                 //Sydney
                 CityView(timeZoneOffset: "+16", city: "Sydney", time: "10:35", amOrPm: "PM")
+            }
+            .navigationTitle("World Clock")
+            .toolbar {
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    
+                    Button("Edit") {
+                        // Does nothing right now
+                    }
+                    
+                }
+                
+                ToolbarItem(placement: .primaryAction) {
+                    Button {
+                    } label: {
+                        Image(systemName: "plus")
+                    }
                 }
             }
-                    .navigationTitle("World Clock")
-                    .toolbar {
-                        
-                        ToolbarItem(placement: .topBarLeading) {
-                            
-                            Button("Edit") {
-                                // Does nothing right now
-                            }
-                            
-                        }
-                        
-                        ToolbarItem(placement: .primaryAction) {
-                            Button {
-                            } label: {
-                                Image(systemName: "plus")
-                            }
-                        }
-                    }
-
-            }
         }
-    
+
+        
+    }
+}
+
 
 #Preview {
     LandingView()
